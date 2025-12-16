185836be292ef486406015aac91bb845276f6cf1
Shern Tee <shernren@gmail.com>
2025-12-16 12:14:12 +1000

    Add pandas to setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'numpy', 'scipy', 'matplotlib'
+    'numpy', 'scipy', 'matplotlib', 'pandas'
     # 'requests', 'maya', 'records',
 ]
 
